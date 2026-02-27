d5da7492e194321a409e057141678f3acfc01785
selvamani <selvamanib986@gmail.com>
2026-02-27 17:49:38 +0530

    selva

--- index.js
+++ index.js
@@ -9,21 +9,14 @@ import { FabricDetailsQuery, orderAllowedDetailsQuery } from "./services/dataInt
 
 
 
-const getorderallowdetintQuery = `SELECT * FROM orderallowdetint WHERE ISCOMPLETE = 0 `
-
-const getFabricIntQuery = ` SELECT * FROM FabricINT WHERE ISCOMPLETE = 0 `
-
-
 // const getorderallowdetintQuery = `SELECT * FROM orderallowdetint WHERE ISCOMPLETE = 1 `
 
 // const getFabricIntQuery = ` SELECT * FROM FabricINT WHERE ISCOMPLETE = 1 `
 
 
-async function getRes(connection, sqlQuery) {
+export async function getRes(connection, sqlQuery) {
     try {
-        // console.log("executing query: ", sqlQuery)
         const result = await connection.execute(sqlQuery)
-        // console.log("result is: ", result)
         return result
     } catch (error) {
         console.log(error);
@@ -31,7 +24,7 @@ async function getRes(connection, sqlQuery) {
     }
 }
 
-async function getResNew(connection, sqlQuery, values) {
+export async function updateIsCompleteFlag(connection, sqlQuery, values) {
     try {
 
         // console.log(sqlQuery, "sqlQuery");
@@ -47,7 +40,7 @@ async function getResNew(connection, sqlQuery, values) {
 }
 
 
-async function constructUpdateQuery1(resp, field) {
+export async function constructUpdateQuery1(resp, field) {
     const ids = [
         ...new Set(
             resp
@@ -69,7 +62,7 @@ async function constructUpdateQuery1(resp, field) {
 }
 
 
-async function constructUpdateQuery2(resp, field) {
+export async function constructUpdateQuery2(resp, field) {
     const ids = [
         ...new Set(
             resp
@@ -84,12 +77,12 @@ async function constructUpdateQuery2(resp, field) {
 
     const inClause = ids.map(id => `'${id}'`).join(', ');
 
-    
+
 
 
     const updateQuery = `
     UPDATE FabricINT
-    SET ISCOMPLETE = 1
+    SET ISCOMPLETE = 1 
     WHERE "srno" IN (${inClause})
   `;
 
@@ -131,137 +124,22 @@ async function main() {
 
 
     let connection1;
-    let connection2;
+    connection1 = await getConnect(dbGetDatabase)
 
     try {
+        
 
 
-        connection1 = await getConnect(dbGetDatabase)
-
-
-        let getDataorderallowdetintQuery = await getRes(connection1, getorderallowdetintQuery);
-
-
-        let getDatagetFabricIntQuery = await getRes(connection1, getFabricIntQuery);
+        await orderAllowedDetailsQuery(connection1)
+        await FabricDetailsQuery(connection1)
 
 
 
 
 
 
-        let response1 = getDataorderallowdetintQuery?.rows?.map((po) => ({
-            srnNo: po[0],
-            erp_no: po[1],
-            orderdate: po[2],
-            product_type: po[3],
-            style_no: po[4],
-            colour: po[5],
-            sizes: po[6],
-            po_no: po[7],
-            po_date: po[8],
-            unit_of_measure: po[9],
-            confirm_qty: po[10],
-            extrapolation: po[11],
-            order_qty: po[12],
-            customer_name: po[13],
-            delivery_date: po[14],
-            season: po[15],
-            product_desc: po[16],
-            ordertime: po[17],
-            item_no: po[18],
-            articlecode: po[19],
-            INSEAM: po[20],
-            description: po[21],
-            country_desc: po[22],
-            sam: po[23],
-            Recd_Date: po[24],
-            ISCOMPLETE: po[25],
-
-        }));
 
 
-
-        let response2 = getDatagetFabricIntQuery?.rows?.map((po) => ({
-            srnNo: po[0],
-            unit_code1: po[1],
-            material_dc: po[2],
-            item_no: po[3],
-            color: po[4],
-            posting_date: po[5],
-            entry_date: po[6],
-            entry_time: po[7],
-            user_id: BigInt(po[8]),
-            qty: po[9],
-            uom: po[10],
-            roll_no: po[11],
-            material: po[12],
-            material_desc: po[13],
-            movement_type: po[14],
-            unit_code: po[15],
-            location: po[16],
-            Supplier_code: po[17],
-            supplier_name: po[18],
-            Recd_Date: po[19],
-            shade: po[20],
-            gsm: po[21],
-            roll_length: po[22],
-            length_mtrs: po[23],
-            actual_width: po[24],
-            sh_length: po[25],
-            sh_width: po[26],
-            grouping: po[27],
-            customer_name: po[28],
-            style_no: po[29],
-
-        }));
-
-
-        // console.log(response1.length, "getDataorderallowdetintQuery")
-        // console.log(response2, "response2.length")
-
-
-        const pushQuery1 = await orderAllowedDetailsQuery(response1);
-
-        const pushQuery2 = await FabricDetailsQuery(response2);
-
-
-
-
-        const updateQuery1 = await constructUpdateQuery1(response1, "erp_no");
-
-        const updateQuery2 = await constructUpdateQuery2(response2, "srnNo");
-
-
-        if (updateQuery1) {
-            const { query } = updateQuery1;
-
-
-
-
-            await getResNew(connection1, query);
-        }
-
-
-        if (updateQuery2) {
-            const { query } = updateQuery2;
-
-
-
-            await getResNew(connection1, query);
-        }
-
-
-
-
-
-
-        // connection2 = await getConnectionForSql(dbPushDataBase)
-
-        // a;
-
-        // await getRes(connection2, pushQuery);
-        // await connection2.commit();
-        // await connection1.commit();
 
 
 
@@ -270,16 +148,12 @@ async function main() {
         if (connection1) {
             await connection1.rollback();
         }
-        if (connection2) {
-            await connection2.rollback();
-        }
+
     } finally {
         if (connection1) {
             await connection1.close()
         }
-        if (connection2) {
-            await connection2.close()
-        }
+
     }
 
 }
--- services/dataInterChange.js
+++ services/dataInterChange.js
@@ -1,54 +1,151 @@
 import { PrismaClient } from '@prisma/client'
+import { constructUpdateQuery1, constructUpdateQuery2, getRes, updateIsCompleteFlag } from '../index.js';
 const prisma = new PrismaClient();
 
 
-export const orderAllowedDetailsQuery = async (resp) => {
-
-    // console.log("resp length", resp[0]);
 
 
-    const mapped = resp?.map(item => ({
-        srnNo: item.srnNo ? parseInt(item.srnNo) : null,
-        erp_no: item.erp_no ? item.erp_no : null,
-        orderdate: item.orderdate ? new Date(item.orderdate) : null,
-        product_type: item.product_type ? item.product_type : null,
-        style_no: item.style_no ? item.style_no : null,
-        colour: item.colour ? item.colour : null,
-        sizes: item.sizes ? item.sizes : null,
-        po_no: item.po_no ? item.po_no : null,
-        po_date: item.po_date ? new Date(item.po_date) : null,
-        unit_of_measure: item.unit_of_measure ? item.unit_of_measure : null,
-        confirm_qty: item.confirm_qty ? parseFloat(item.confirm_qty) : null,
-        extrapolation: item.extrapolation ? parseFloat(item.extrapolation) : null,
-        order_qty: item.order_qty ? parseFloat(item.order_qty) : null,
-        customer_name: item.customer_name ? item.customer_name : null,
-        delivery_date: item.delivery_date ? new Date(item.delivery_date) : null,
-        season: item.season ? item.season : null,
-        product_desc: item.product_desc ? item.product_desc : null,
-        ordertime: item.ordertime ? item.ordertime : null,
-        item_no: item.item_no ? parseFloat(item.item_no) : null,
-        articlecode: item.articlecode ? item.articlecode : null,
-        INSEAM: item.INSEAM ? item.INSEAM : null,
-        description: item.description ? item.description : null,
-        country_desc: item.country_desc ? item.country_desc : null,
-        sam: item.sam ? parseFloat(item.sam) : null,
-        Recd_Date: item.Recd_Date ? new Date(item.Recd_Date) : null,
-        ISCOMPLETE: item.ISCOMPLETE ? Boolean( item.ISCOMPLETE) : null,
-    }));
 
-    const result = await prisma.OrderAllowDetInt.createMany({
-        data: mapped,
-    });
+const getorderallowdetintQuery = `SELECT * FROM orderallowdetint WHERE ISCOMPLETE = 0 `
 
-    return result;
+const getFabricIntQuery = ` SELECT * FROM FabricINT WHERE ISCOMPLETE = 0 `
+
+
+
+export const orderAllowedDetailsQuery = async (connection) => {
+
+
+    try {
+        let getDataorderallowdetintQuery = await getRes(connection, getorderallowdetintQuery);
+
+
+        let unFlateDataorderallowdetint = getDataorderallowdetintQuery?.rows?.map((po) => ({
+            srnNo: po[0],
+            erp_no: po[1],
+            orderdate: po[2],
+            product_type: po[3],
+            style_no: po[4],
+            colour: po[5],
+            sizes: po[6],
+            po_no: po[7],
+            po_date: po[8],
+            unit_of_measure: po[9],
+            confirm_qty: po[10],
+            extrapolation: po[11],
+            order_qty: po[12],
+            customer_name: po[13],
+            delivery_date: po[14],
+            season: po[15],
+            product_desc: po[16],
+            ordertime: po[17],
+            item_no: po[18],
+            articlecode: po[19],
+            INSEAM: po[20],
+            description: po[21],
+            country_desc: po[22],
+            sam: po[23],
+            Recd_Date: po[24],
+            ISCOMPLETE: po[25],
+
+        }));
+
+
+
+        const mapped = unFlateDataorderallowdetint?.map(item => ({
+            srnNo: item.srnNo ? parseInt(item.srnNo) : null,
+            erp_no: item.erp_no ? item.erp_no : null,
+            orderdate: item.orderdate ? new Date(item.orderdate) : null,
+            product_type: item.product_type ? item.product_type : null,
+            style_no: item.style_no ? item.style_no : null,
+            colour: item.colour ? item.colour : null,
+            sizes: item.sizes ? item.sizes : null,
+            po_no: item.po_no ? item.po_no : null,
+            po_date: item.po_date ? new Date(item.po_date) : null,
+            unit_of_measure: item.unit_of_measure ? item.unit_of_measure : null,
+            confirm_qty: item.confirm_qty ? parseFloat(item.confirm_qty) : null,
+            extrapolation: item.extrapolation ? parseFloat(item.extrapolation) : null,
+            order_qty: item.order_qty ? parseFloat(item.order_qty) : null,
+            customer_name: item.customer_name ? item.customer_name : null,
+            delivery_date: item.delivery_date ? new Date(item.delivery_date) : null,
+            season: item.season ? item.season : null,
+            product_desc: item.product_desc ? item.product_desc : null,
+            ordertime: item.ordertime ? item.ordertime : null,
+            item_no: item.item_no ? parseFloat(item.item_no) : null,
+            articlecode: item.articlecode ? item.articlecode : null,
+            INSEAM: item.INSEAM ? item.INSEAM : null,
+            description: item.description ? item.description : null,
+            country_desc: item.country_desc ? item.country_desc : null,
+            sam: item.sam ? parseFloat(item.sam) : null,
+            Recd_Date: item.Recd_Date ? new Date(item.Recd_Date) : null,
+            ISCOMPLETE: item.ISCOMPLETE ? Boolean(item.ISCOMPLETE) : null,
+        }));
+
+
+
+        const updateQuery1 = await constructUpdateQuery1(unFlateDataorderallowdetint, "erp_no");
+        const { query } = updateQuery1;
+
+
+
+        const result = await prisma.$transaction(async (tx) => {
+            const resut = await tx.OrderAllowDetInt.createMany({
+                data: mapped,
+            })
+            await updateIsCompleteFlag(connection, query);
+        })
+
+
+        return result;
+    } catch (e) {
+        connection.rollback()
+    }
+
+
+
 };
 
 
 
-export const FabricDetailsQuery = async (resp) => {
+export const FabricDetailsQuery = async (connection) => {
 
 
-    const mapped = resp?.map(item => ({
+    let getDatagetFabricIntQuery = await getRes(connection, getFabricIntQuery);
+
+    let unFlatDatagetFabricIntQuery = getDatagetFabricIntQuery?.rows?.map((po) => ({
+        srnNo: po[0],
+        unit_code1: po[1],
+        material_dc: po[2],
+        item_no: po[3],
+        color: po[4],
+        posting_date: po[5],
+        entry_date: po[6],
+        entry_time: po[7],
+        user_id: (po[8]),
+        qty: po[9],
+        uom: po[10],
+        roll_no: po[11],
+        material: po[12],
+        material_desc: po[13],
+        movement_type: po[14],
+        unit_code: po[15],
+        location: po[16],
+        Supplier_code: po[17],
+        supplier_name: po[18],
+        Recd_Date: po[19],
+        shade: po[20],
+        gsm: po[21],
+        roll_length: po[22],
+        length_mtrs: po[23],
+        actual_width: po[24],
+        sh_length: po[25],
+        sh_width: po[26],
+        grouping: po[27],
+        customer_name: po[28],
+        style_no: po[29],
+
+    }));
+
+    const mapped = unFlatDatagetFabricIntQuery?.map(item => ({
 
 
 
@@ -84,9 +181,21 @@ export const FabricDetailsQuery = async (resp) => {
         style_no: item.style_no ? item.style_no : null,
     }));
 
-    const result = await prisma.FabricINT.createMany({
-        data: mapped,
-    });
+
+
+    const updateQuery2 = await constructUpdateQuery2(unFlatDatagetFabricIntQuery, "srnNo");
+    const { query } = updateQuery2;
+
+
+    const result = await prisma.$transaction(async (tx) => {
+        const resut = await tx.FabricINT.createMany({
+            data: mapped,
+        })
+        await updateIsCompleteFlag(connection, query);
+
+    })
+
+
 
     return result;
 };
